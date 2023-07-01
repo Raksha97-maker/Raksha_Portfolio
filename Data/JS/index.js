ab42145191bbc4b69043035efacdd5800c7f5a02
Raksha97-maker <rakshakusumakar789@gmail.com>
2023-07-01 13:18:25 +0530

    Portfolio

--- Data/JS/index.js
+++ Data/JS/index.js
@@ -14,6 +14,16 @@ scrollUp.addEventListener("click", () => {
 });
 // Nav hamburgerburger selections
 
+const check=document.getElementById("check");
+check.addEventListener("click",function(){
+    window.location.href="https://flourishing-cucurucho-4763d6.netlify.app/index.html" 
+})
+
+const check1=document.getElementById("check1");
+check1.addEventListener("click",function(){
+    window.location.href="https://649ed83dad3e52109665e5f9--moonlit-kitsune-a613b0.netlify.app/"
+})
+
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
